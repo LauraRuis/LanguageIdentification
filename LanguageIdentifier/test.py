b9bae89d907794f594b75ded7c177ad5114f6a97
vdankers <verna.dankers@student.uva.nl>
2018-09-15 01:59:31 +0200

    Test function to compute accuracy on test set

---
+++ LanguageIdentifier/test.py
@@ -0,0 +1,26 @@
+import torch
+import os
+import sys
+import numpy
+
+from torchtext.data import Iterator
+from model import Model
+
+
+def test(model : Model, testing_data : Iterator) -> float:
+
+    model.eval()
+    batch_accuracies = []
+
+    for j, batch in enumerate(iter(testing_data)):
+        # Show progress on test data
+        print("Test batch: {}          ".format(j), end='\r')
+        sys.stdout.flush()
+        characters = torch.autograd.Variable(batch.characters[0])
+        languages = batch.language
+        predictions = model.forward(characters)
+        _, predicted_languages = torch.topk(predictions, 1)
+
+        # Save data needed to calculate accuracy for later
+        batch_accuracies.extend(languages.eq(predicted_languages))
+    return numpy.array([sample.item() for sample in batch_accuracies]).mean()
